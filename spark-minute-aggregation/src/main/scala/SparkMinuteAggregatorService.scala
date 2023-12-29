b9fd8c7a007161d2b3c48dbd023ef3fb6e99b2c7
pedrolera <pedromfalera@gmail.com>
2023-12-29 01:54:00 +0000

    minute aggregation bugfix

--- spark-minute-aggregation/src/main/scala/SparkMinuteAggregatorService.scala
+++ spark-minute-aggregation/src/main/scala/SparkMinuteAggregatorService.scala
@@ -2,7 +2,7 @@ import com.typesafe.scalalogging.LazyLogging
 import common.model.SparkConfig
 import common.utils.SparkUtils
 import org.apache.spark.sql.{DataFrame, SparkSession, functions}
-import org.apache.spark.sql.functions.{col, collect_list, count, countDistinct, date_trunc, first, struct, sum, when, window}
+import org.apache.spark.sql.functions.{col, collect_list, count, countDistinct, date_trunc, first, from_unixtime, struct, sum, when, window}
 
 import scala.util.Try
 
@@ -37,15 +37,25 @@ object SparkMinuteAggregatorService extends SparkUtils with LazyLogging {
   // Modified aggregatePurchaseDF function
   def aggregatePurchaseDF(purchaseDF: DataFrame): DataFrame = {
     purchaseDF
+      //build the key timestamp for the mongo table from the time column that is bigInt type
+      .withColumn("formattedTimestamp", from_unixtime(col("time")))
+      //build watermark from the timestamp field since its already timestamp type.
       .withWatermark("timestamp", "10 minutes")
+      //aggregate by the timestamp with a window of 1 minute
       .groupBy(window(col("timestamp"), "1 minute"))
       .agg(
+        //get the total count of purchases
         count("*").as("purchaseCount"),
+        //get the total ammount of purchase value
         sum("purchaseValue").as("totalPurchaseValue"),
-        first("date").as("date")
+        //keep the first formattedTimestamp to use as a key for the table
+        first("formattedTimestamp").as("formattedTimestamp")
       )
+      //prepare the userData field
       .withColumn("userData", struct(col("totalPurchaseValue"), col("purchaseCount")))
-      .withColumn("minute", date_trunc("minute", col("date")))
-      .select(col("minute").as("timestamp"), col("userData"))
+      //date_trunc to the minute for good looking information on the db
+      .withColumn("timestamp", date_trunc("minute", col("formattedTimestamp")))
+      //select fields to insert on the
+      .select(col("timestamp"), col("userData"))
   }
 }
